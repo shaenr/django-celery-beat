a6742833456e8f1da989284fca1bafef2692f335
shaenr <shaenr@slackmanic.com>
2022-07-24 06:27:22 -0500

    https://github.com/testdrivenio/django-celery

--- project/core/settings.py
+++ project/core/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "/staticfiles/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+
